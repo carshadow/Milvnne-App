e68d8a32efd24d7c4951d8dbcea40c6ae6330fa8
Carlos Sanchez <carlos.sanchez.castillo2003@gmail.com>
2025-04-30 21:11:22 -0400

    updated user csrf import

--- clothing-backend/routes/userRoutes.js
+++ clothing-backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
-import csrfProtection from "../middlewares/csrfMiddleware.js"; // 👈 Asegúrate que esto exista
+import { csrfProtection } from "../middlewares/csrfMiddleware.js"; // 👈 Asegúrate que esto exista
 import crypto from "crypto";
 import nodemailer from "nodemailer";
 import { z } from "zod";
