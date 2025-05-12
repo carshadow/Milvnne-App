650b7bd15c22d0293b2b6ad96eb4b581a1b95b1a
Carlos Sanchez <carlos.sanchez.castillo2003@gmail.com>
2025-05-12 01:57:07 -0400

    Fixed login error in the server

--- clothing-backend/server.js
+++ clothing-backend/server.js
@@ -43,6 +43,8 @@ app.options("*", cors({
 // ✅ Header extra para cookies cross-origin
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, CSRF-Token");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
