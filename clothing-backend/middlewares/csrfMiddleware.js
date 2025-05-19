9e87cce7ddee9d080566de2d4c6e5ff1a6b4d047
Carlos Sanchez <carlos.sanchez.castillo2003@gmail.com>
2025-05-19 18:16:38 -0400

    fixed put csrf

--- clothing-backend/middlewares/csrfMiddleware.js
+++ clothing-backend/middlewares/csrfMiddleware.js
@@ -1,6 +1,11 @@
 import csrf from "csurf";
 
 export const csrfProtection = csrf({
-    cookie: false,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        signed: true,
+    },
     ignoreMethods: ["GET", "HEAD", "OPTIONS"],
 });
