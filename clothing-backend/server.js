a3dee3f71757c53ae2d21a08cea1662ee7efccad
Carlos Sanchez <carlos.sanchez.castillo2003@gmail.com>
2025-05-19 18:08:38 -0400

    fixed port listening

--- clothing-backend/server.js
+++ clothing-backend/server.js
@@ -19,7 +19,7 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:3000",
     "https://brand-app.fly.dev",
-    "https://clothing-backend.fly.dev/"
+    "https://clothing-backend.fly.dev"
 
 ];
 
